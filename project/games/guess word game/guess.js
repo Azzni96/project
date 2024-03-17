69246091ce9cd85079a5cd3b0b866133facbe711
Azzni96 <nihadazzam96@gmail.com>
2024-03-17 17:20:14 +0200

    start

---
+++ project/games/guess word game/guess.js
@@ -0,0 +1,4 @@
+let gameName = "Guess The word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector("footer").innerHTML = `${gameName} Game Created by Nihad web school
